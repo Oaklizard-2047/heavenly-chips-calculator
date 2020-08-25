d1c8fbd416618185fe167cb6ebd0a834b85bc8d8
Oaklizard <oaklizardmain@gmail.com>
2020-08-26 07:52:33 +0800

    added main functionality

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+using namespace std;
+int main()
+{
+	double hcgoal;
+	cout << "Heavenly Chips Calculator\n==============================\n";
+	cin >> hcgoal;
+	cout.setf(ios::fixed);
+	cout << setprecision(0) << (pow(10.0,12.0))*(pow(hcgoal,3.0)) << "\n";
+}
