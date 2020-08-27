f74edd56ce7b5a2f17675824d8167d66abc3689c
Oaklizard <58917335+Oaklizard-2047@users.noreply.github.com>
2020-08-27 15:31:42 +0800

    Makes the window doesnt close immidietly

--- source/main.cpp
+++ source/main.cpp
@@ -6,7 +6,9 @@ int main()
 {
 	double hcgoal;
 	cout << "Heavenly Chips Calculator\n==============================\n";
-	cin >> hcgoal;
-	cout.setf(ios::fixed);
-	cout << setprecision(0) << (pow(10.0,12.0))*(pow(hcgoal,3.0)) << "\n";
+	while(true){
+		cin >> hcgoal;
+		cout.setf(ios::fixed);
+		cout << setprecision(0) << (pow(10.0,12.0))*(pow(hcgoal,3.0)) << "\n";
+	}
 }
