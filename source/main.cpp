f645e1c4bbeceebb9ac3fb24b81b851fd17a4975
Oaklizard <58917335+Oaklizard-2047@users.noreply.github.com>
2020-08-27 16:22:48 +0800

    fixes a bug where entering a string causes an infinity loop

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <math.h>
 #include <iomanip>
+#include <limits>
 using namespace std;
 int main()
 {
@@ -8,7 +9,14 @@ int main()
 	cout << "Heavenly Chips Calculator\n==============================\n";
 	while(true){
 		cin >> hcgoal;
-		cout.setf(ios::fixed);
-		cout << setprecision(0) << (pow(10.0,12.0))*(pow(hcgoal,3.0)) << "\n";
+		if(!cin){
+			cin.clear();
+			cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			cout << "Please enter a number\n";
+		}
+		else{
+			cout.setf(ios::fixed);
+			cout << setprecision(0) << (pow(10.0,12.0))*(pow(hcgoal,3.0)) << "\n";
+		}
 	}
 }
